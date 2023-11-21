c391ea78f14a56aaca02b3e791d7e917ab330d4b
vamansri <108322483+vamansri@users.noreply.github.com>
2023-11-21 17:58:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import WebPage1 from './WebPage1';
+import WebPage2 from './WebPage2';
+import WebPage3 from './WebPage3';
+// import MyReactComponent from './MyReactComponent'
+import WebPage4 from './WebPage4';
+import WebPage5 from './WebPage5';
+// import WebPage6 from './WebPage6';
+
+function App() {
+  return (
+    <div className="App">
+
+
+
+
+
+      <WebPage1/>
+      {/* <WebPage6/> */}
+      <WebPage2/>
+      <WebPage3/>
+      <WebPage4/>
+      <WebPage5/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
